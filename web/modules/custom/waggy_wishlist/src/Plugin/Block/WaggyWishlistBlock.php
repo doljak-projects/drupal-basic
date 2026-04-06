599d96dfb92af28f15db5f884099a265b385037f
Doljak <jedoljak@gmail.com>
2026-04-06 17:19:39 -0300

    fix: add wishlist route and header link

--- web/modules/custom/waggy_wishlist/src/Plugin/Block/WaggyWishlistBlock.php
+++ web/modules/custom/waggy_wishlist/src/Plugin/Block/WaggyWishlistBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\waggy_wishlist\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
 
 /**
  * @Block(
@@ -15,6 +16,7 @@ class WaggyWishlistBlock extends BlockBase {
   public function build() {
     return [
       '#theme' => 'waggy_wishlist',
+      '#wishlist_url' => Url::fromRoute('waggy_wishlist.page')->toString(),
     ];
   }
 
